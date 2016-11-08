d6a14e6e7df2292ecfab1225c16ee858c73d9cfd
tomapter <tom@eccmperformance.com>
2016-11-08 18:49:33 +0000

    firstcommit

---
+++ App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tomapter.findyourpub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.cities);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+}
---
+++ App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/PubDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.tomapter.findyourpub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PubDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pub_details);
+    }
+}
