f038c3d50b82ec9aa65e1d0fae16ff8f6527c572
tomapter <tom@eccmperformance.com>
2016-11-16 12:04:35 +0000

    merge

--- App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/MainActivity.java
+++ App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/MainActivity.java
@@ -4,30 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_me);
-
-        Spinner spinner = (Spinner) findViewById(R.id.cities);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.cities, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        setContentView(R.layout.activity_main);
     }
 
     public void onClick(View v){
         switch (v.getId()){
             case R.id.find_me:
+                startActivity(new Intent(this, FindPubActivity.class));
                 break;
             case R.id.go:
                 startActivity(new Intent(this, PubListActivity.class));
