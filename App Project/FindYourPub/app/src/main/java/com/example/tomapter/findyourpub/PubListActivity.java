cd8a94b159ea20c870d8c97bbac30d7737c28ce8
tomapter <tom@eccmperformance.com>
2016-11-17 12:52:09 +0000

    merge

--- App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/PubListActivity.java
+++ App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/PubListActivity.java
@@ -1,39 +1,144 @@
 package com.example.tomapter.findyourpub;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+
+import com.example.tomapter.findyourpub.dummy.DummyContent;
+
+import java.util.List;
+
+/**
+ * An activity representing a list of Pubs. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link PubDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
 public class PubListActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private String[] pubNames;
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    private String [] pubNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        pubNames = getResources().getStringArray(R.array.pub_name_coventry);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pub_list);
 
-        pubNames = getResources().getStringArray(R.array.pub_name_coventry);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
 
-        listView = (ListView) findViewById(R.id.pub_name_list);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pubNames);
-        listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Toast.makeText(getBaseContext(), pubNames[position] + " is a good pub!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-        );
+        View recyclerView = findViewById(R.id.pub_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
 
+        if (findViewById(R.id.pub_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
     }
 
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+    }
 
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.pub_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(PubDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        PubDetailFragment fragment = new PubDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.pub_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, PubDetailActivity.class);
+                        intent.putExtra(PubDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
 }
--- App Project/FindYourPub/app/src/main/java/com/example/tomapter/findyourpub/PubNameAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.tomapter.findyourpub;
-
-/**
- * Created by aptert on 10/11/2016.
- */
-public class PubNameAdapter {
-}
